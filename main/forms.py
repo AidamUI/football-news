3e68b600e748b7f015a35a865b0aba2c000f232d
aidam <aidamkaila@gmail.com>
2025-10-01 14:08:02 +0700

    tutorial 5

--- main/forms.py
+++ main/forms.py
@@ -1,10 +1,19 @@
 from django.forms import ModelForm
 from main.models import News
+from django.utils.html import strip_tags
 
 class NewsForm(ModelForm):
     class Meta:
         model = News
         fields = ["title", "content", "category", "thumbnail", "is_featured"]
+
+    def clean_title(self):
+        title = self.cleaned_data["title"]
+        return strip_tags(title)
+
+    def clean_content(self):
+        content = self.cleaned_data["content"]
+        return strip_tags(content)
 
 
 # model = News untuk menunjukkan model yang akan digunakan untuk form. Ketika data dari form disimpan, isi dari form akan disimpan menjadi sebuah objek News.
